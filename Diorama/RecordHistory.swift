49962b390c55acfe13b82a65bfbb2e463353b251
leahchen <leahchen0105@gmail.com>
2024-06-09 02:42:36 +0800

    Add new source code. Already modified some place to add speech recognize feature.

---
+++ Diorama/RecordHistory.swift
@@ -0,0 +1,22 @@
+//
+//  RecordHistory.swift
+//  Diorama
+//
+//  Created by 陳詠恩 on 2024/6/9.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let position: String?
+    var transcript: String?
+    
+    init(id: UUID = UUID(), position: String? = nil, transcript: String? = nil) {
+        self.id = id
+        self.position = position
+        self.transcript = transcript
+    }
+}
